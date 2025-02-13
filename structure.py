f3fb1938e59b1f3c1974d3efeacda73b0f8c9c81
Anika <apatel909@gatech.edu>
2025-02-13 13:41:35 -0500

    finished structure

--- structure.py
+++ structure.py
@@ -1,40 +1,58 @@
 import numpy as np
-    """
-    
-    
-        NOT DONE YET!!!
-    
-    
-    """
+import networkx as nx
+import dmc
+
+"""
+
+
+    NOT DONE YET!!!
+
+
+"""
 class DMC_structure:
-    def __init__(self, graph, functionNames, goals):
-        """_summary_
-
+    def __init__(self, DMCconnectionList):
+        """_summary_  
         Args:
-            graph (_type_): a graph of nodes. DMC's labeled in numerical order
-            functionNames (_type_): list of functionNames in numerical order;
-            goals (_type_): list of goals in numerical order
-        """
-        self.graph = graph
-        self.goals = goals
-        self.functionNames = functionNames
-        self.initialized = False
-    
-    def BFS(self, s):
-        visited = [False] * (max(self.graph) + 1)
-        queue = []
-        queue.append(s)
-        visited[s] = True
-
-        while queue:
-            s = queue.pop(0)
-            if(self.initialized == False) {
-                
-            }
+            DMCconnectionList: array with the following specifications:
             
-            for i in self.graph[s]:
-                if not visited[i]:
-                    queue.append(i)
-                    visited[i] = True
-        
+            DMC Number, connected DMCs, DMC function name, goal, constraints, parameters, DMC input
+            ex: 
+            [[0, [1], PID, 25, [1, 0, 0], [20, ...], [25, 6, 1]],
+             [1, [2], random, 10, [0, 10, 21], [10, ...], [5, 20, 100]],
+             ]
+             
+             0 is our starting DMC
+             2 is our output DMC
+        """
+        self.DMCconnectionList = DMCconnectionList
+
     
+    """_summary_
+        iterate through the DMC's and update them
+        they all update at the same time, 
+        and you return the updated list of DMC structure parameters
+        
+        
+    """
+    def iterate(self):
+        # make copy of list
+        newDMCconnectionList = self.DMCconnectionList
+        output = []
+        for i in range(len(self.DMCconnectionList)):
+            # output = DMC(input)
+            arr = self.DMCconnectionList[i]
+            DMCparams = arr[2:6]
+            DMCinput = arr[6]
+            output = dmc(*(DMCparams)).update(DMCinput)
+            
+            # update DMCs that need the output
+            # CURRENT ASSUMPTION - if two DMC's point to the same thing, 
+            #   just do a basic override.
+            
+            connectedDMCs = arr[1]
+            for adjDMC in connectedDMCs:
+                newDMCconnectionList[adjDMC][-1] = output
+        
+        # update DMCconnectionList
+        self.DMCconnectionList = newDMCconnectionList
+        return output
